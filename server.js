44c189e6f10fd631d11830a451e1eeb5c08e6f1f
Rohan Singh <rohan.singh@codemonk.in>
2022-09-30 12:37:20 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const pool = require('./db')
+const DATA = require('./src/DATA.json')
+//middleware
+app.use(cors())
+app.use(express.json());
+
+//routes
+
+
+
+app.get('/rent', async (req, res) => {
+    try {
+        const allRent = await pool.query('SELECT * FROM rent')
+        res.json(allRent.rows)
+    }
+    catch (error) {
+        console.log(error)
+    }
+})
+
+
+
+app.get('/search', async (req, res) => {
+    const SEARCH_OBJ = {
+        equal: '=',
+        greater: '>',
+        less: '<',
+
+    }
+    try {
+        const { column, filterBy, filterSearch } = req.query;
+        const rent = await pool.query(`SELECT * FROM rent WHERE ${column} ${SEARCH_OBJ[filterBy]} $1`, [filterSearch]);
+        res.json(rent.rows)
+
+
+    } catch (error) {
+        console.log(error)
+    }
+})
+
